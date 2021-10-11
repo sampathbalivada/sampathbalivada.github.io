ecd7297807ed2899817e2e2f0ae9802384e2fa69
sampathbalivada <balivadask2000@gmail.com>
2021-10-11 23:52:42 +0530

    under construction message

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Landing Page Under Construction</h1>
+	<p>Visit <a href="https://blog.sampath.dev">blog.sampath.dev</a> in the mean time.</p>
 </main>
 
 <style>
